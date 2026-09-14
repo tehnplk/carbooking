44cb7e0f279b821aedb33fc34294d312b2049fce
tehnplk <tehnplk@gmail.com>
2026-09-14 12:45:57 +0700

    chore: align env configuration and derive NEXTAUTH_URL and SSO_REDIRECT_URI

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
+if (process.env.AUTH_URL) {
+  process.env.NEXTAUTH_URL = process.env.AUTH_URL;
+  process.env.SSO_REDIRECT_URI = new URL("/auth/sso/callback", process.env.AUTH_URL).toString();
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
